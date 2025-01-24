2927828f2e4849aed6fa14e2ff1b39c041e2ceb0
nozibuddowla <nozibuddowla@gmail.com>
2025-01-25 01:22:54 +0600

    add view contacts features

--- contacts/urls.py
+++ contacts/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('add/', views.add_contact, name='add_contact'),
+    path('', views.contact_list, name='contact_list'),
+    path('contacts/<int:pk>/', views.contact_detail, name='contact_detail'),
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import ContactForm
+from .models import Contact
 
 # Create your views here.
 def add_contact(request):
@@ -11,3 +12,11 @@ def add_contact(request):
     else:
         form = ContactForm()
     return render(request, "contacts/add_contact.html", {"form": form})
+
+def contact_list(request):
+    contacts = Contact.objects.all()
+    return render(request, "contacts/contact_list.html", {"contacts": contacts})
+
+def contact_detail(request, pk):
+    contact = Contact.objects.get(pk=pk)
+    return render(request, "contacts/contact_detail.html", {"contact": contact})
